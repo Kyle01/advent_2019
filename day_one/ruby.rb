394dce48567e90e9369a9405a3c03276986b19cb
kyle01 <kylemcv@gmail.com>
2019-12-01 10:40:46 -0700

    Complete ruby 1.2

--- day_one/ruby.rb
+++ day_one/ruby.rb
@@ -1,3 +1,4 @@
+## Part 1
 file = File.open('input.txt')
 weights = file.readlines.map(&:chomp)
 sum = 0
@@ -10,5 +11,19 @@ weights.each do |weight|
     sum += fuel_required(weight.to_i)
 end
 
-puts sum
+# puts sum
 
+# Part 2
+
+def total_fuel_required(mass)
+    fuel = fuel_required(mass)
+    return 0 if fuel <= 0
+    fuel += total_fuel_required(fuel)
+end
+
+sum = 0
+weights.each do |weight|
+    sum +=  total_fuel_required(weight.to_i)
+end
+
+puts sum
